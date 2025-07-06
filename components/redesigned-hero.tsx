dddb54310034452f7a6a460487717b2f825652ee
russs743 <142502713+russs743@users.noreply.github.com>
2025-07-06 10:06:21 +0700

    Update redesigned-hero.tsx

--- components/redesigned-hero.tsx
+++ components/redesigned-hero.tsx
@@ -62,10 +62,9 @@ export default function RedesignedHero() {
                 bagian desain, khususnya <span className="text-primary font-semibold">UI/UX design</span> untuk web.
               </p>
 
-              
               <h2 className="text-xl md:text-2xl lg:text-3xl text-muted-foreground font-medium">
                 Disclaimer, Pembuatan web ini dan beberapa project 
-                <span className="text-primary font-semibold">dibantu oleh ai</span>
+                <span className="text-primary font-semibold"> dibantu oleh ai </span>
               </h2>
               
             </motion.div>
